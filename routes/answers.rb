ac51aad34043ba046f4189ab06f6579d81cd7b0c
Chris Hurt <chrishcoding@gmail.com>
2019-08-02 09:51:04 +1000

    Teacher review process added

--- routes/answers.rb
+++ routes/answers.rb
@@ -7,3 +7,28 @@ get '/answers' do
     @student_answers = StudentAnswer.all.where(student_id: @student.id)
     erb :student_answers
 end
+
+get '/answers/review' do
+    if loggedIn? && session[:user_type] == 'Teacher'
+        teachers_questions = current_user.latin_questions
+        @review_questions = []
+        teachers_questions.each do |teacher_question|
+            @review_questions << teacher_question.student_answers   
+        end
+        @review_questions.flatten!
+    elsif loggedIn? && session[:user_type] == 'Student'
+
+    end
+    
+    # @student_answers = StudentAnswer.where(student_id: @student.id)
+    erb :answers_review
+end
+
+put '/answers:id' do 
+    student_answer = StudentAnswer.find_by(id: params[:id])
+    student_answer.outcome = params[:outcome]
+    student_answer.reviewed = true
+    student_answer.save
+
+    redirect '/answers/review'
+end
